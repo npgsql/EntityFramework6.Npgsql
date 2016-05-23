a3315508bf306c9d3c3c8d6d05e5145daab121c1
Shay Rojansky <roji@roji.org>
2016-05-23 19:34:33 +0300

    Versioning

---
+++ bump.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+  echo "usage: bump.sh <version>"
+  exit 1
+fi
+
+v=$1
+if [[ $v == *"-" ]]; then
+  echo "Version must not end with -"
+  exit 1
+fi
+
+echo "echo ##teamcity[buildNumber '$v-%1']" > teamcity_set_version.cmd
+
+if [[ $v == *"-"* ]]; then
+  # Prerelease version
+
+  without_prerelease=`echo $v | cut -d- -f1`
+
+  sed -i 's/^\(\s*\)<version>[^<]*<\/version>/\1<version>$version$<\/version>/' src/EntityFramework6.Npgsql/EntityFramework6.Npgsql.nuspec
+  sed -i 's/^\(\s*\)<version>[^<]*<\/version>/\1<version>$version$<\/version>/' src/EntityFramework5.Npgsql/EntityFramework5.Npgsql.nuspec
+
+  sed -i 's/AssemblyVersion("[^"]*")/AssemblyVersion("'$without_prerelease'")/' src/CommonAssemblyInfo.cs
+  sed -i 's/AssemblyFileVersion("[^"]*")/AssemblyFileVersion("'$without_prerelease'")/' src/CommonAssemblyInfo.cs
+  sed -i 's/AssemblyInformationalVersion("[^"]*")/AssemblyInformationalVersion("'$v'")/' src/CommonAssemblyInfo.cs
+else
+  # Release version
+
+  sed -i 's/^\(\s*\)<version>[^<]*<\/version>/\1<version>'$v'<\/version>/' src/EntityFramework6.Npgsql/EntityFramework6.Npgsql.nuspec
+  sed -i 's/^\(\s*\)<version>[^<]*<\/version>/\1<version>'$v'<\/version>/' src/EntityFramework5.Npgsql/EntityFramework5.Npgsql.nuspec
+
+  sed -i 's/AssemblyVersion("[^"]*")/AssemblyVersion("'$v'")/' src/CommonAssemblyInfo.cs
+  sed -i 's/AssemblyFileVersion("[^"]*")/AssemblyFileVersion("'$v'")/' src/CommonAssemblyInfo.cs
+  sed -i 's/AssemblyInformationalVersion("[^"]*")/AssemblyInformationalVersion("'$v'")/' src/CommonAssemblyInfo.cs
+fi
+
+git add teamcity_set_version.cmd
+git add src/EntityFramework6.Npgsql/EntityFramework6.Npgsql.nuspec
+git add src/EntityFramework5.Npgsql/EntityFramework5.Npgsql.nuspec
+git add src/CommonAssemblyInfo.cs
+
+git commit -m "Bump version to $v"
