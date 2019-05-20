17ed69a6d364187f0172cbe47a89325a5728413c
Shay Rojansky <roji@roji.org>
2019-05-20 15:06:09 +0200

    Remove license header from source files

--- src/EntityFramework6.Npgsql/NpgsqlMigrationSqlGenerator.cs
+++ src/EntityFramework6.Npgsql/NpgsqlMigrationSqlGenerator.cs
@@ -1,27 +1,4 @@
-﻿#region License
-// The PostgreSQL License
-//
-// Copyright (C) 2016 The Npgsql Development Team
-//
-// Permission to use, copy, modify, and distribute this software and its
-// documentation for any purpose, without fee, and without a written
-// agreement is hereby granted, provided that the above copyright notice
-// and this paragraph and the following two paragraphs appear in all copies.
-//
-// IN NO EVENT SHALL THE NPGSQL DEVELOPMENT TEAM BE LIABLE TO ANY PARTY
-// FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,
-// INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
-// DOCUMENTATION, EVEN IF THE NPGSQL DEVELOPMENT TEAM HAS BEEN ADVISED OF
-// THE POSSIBILITY OF SUCH DAMAGE.
-//
-// THE NPGSQL DEVELOPMENT TEAM SPECIFICALLY DISCLAIMS ANY WARRANTIES,
-// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-// AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS
-// ON AN "AS IS" BASIS, AND THE NPGSQL DEVELOPMENT TEAM HAS NO OBLIGATIONS
-// TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-#endregion
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity.Migrations.Model;
 using System.Data.Entity.Migrations.Sql;
@@ -436,7 +413,7 @@ namespace Npgsql
         string GetSchemaNameFromFullTableName(string tableFullName)
         {
             var dotIndex = tableFullName.IndexOf('.');
-            return dotIndex != -1 ? tableFullName.Remove(dotIndex) : "dto";   
+            return dotIndex != -1 ? tableFullName.Remove(dotIndex) : "dto";
             //TODO: Check always setting dto schema if no schema in table name is not bug
         }
 
--- test/EntityFramework6.Npgsql.Tests/Support/EntityFrameworkTestBase.cs
+++ test/EntityFramework6.Npgsql.Tests/Support/EntityFrameworkTestBase.cs
@@ -1,27 +1,4 @@
-﻿#region License
-// The PostgreSQL License
-//
-// Copyright (C) 2016 The Npgsql Development Team
-//
-// Permission to use, copy, modify, and distribute this software and its
-// documentation for any purpose, without fee, and without a written
-// agreement is hereby granted, provided that the above copyright notice
-// and this paragraph and the following two paragraphs appear in all copies.
-//
-// IN NO EVENT SHALL THE NPGSQL DEVELOPMENT TEAM BE LIABLE TO ANY PARTY
-// FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,
-// INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
-// DOCUMENTATION, EVEN IF THE NPGSQL DEVELOPMENT TEAM HAS BEEN ADVISED OF
-// THE POSSIBILITY OF SUCH DAMAGE.
-//
-// THE NPGSQL DEVELOPMENT TEAM SPECIFICALLY DISCLAIMS ANY WARRANTIES,
-// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-// AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS
-// ON AN "AS IS" BASIS, AND THE NPGSQL DEVELOPMENT TEAM HAS NO OBLIGATIONS
-// TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-#endregion
-
-using Npgsql;
+﻿using Npgsql;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -202,7 +179,7 @@ namespace EntityFramework6.Npgsql.Tests
         public IQueryable<Blog> GetBlogsByName(string name)
         {
             ObjectParameter nameParameter = new ObjectParameter("Name", name);
-            
+
             return ((IObjectContextAdapter)this).ObjectContext.CreateQuery<Blog>(
                 $"[GetBlogsByName](@Name)", nameParameter);
         }
@@ -337,7 +314,7 @@ namespace EntityFramework6.Npgsql.Tests
                 {
                     IsFunctionImport = true,
                     IsComposable = true,
-                    Parameters = new[] 
+                    Parameters = new[]
                     {
                         FunctionParameter.Create("Name", stringPrimitiveType, ParameterMode.In)
                     },
@@ -345,7 +322,7 @@ namespace EntityFramework6.Npgsql.Tests
                     {
                         FunctionParameter.Create("ReturnType1", modelBlogConceptualType.GetCollectionType(), ParameterMode.ReturnValue)
                     },
-                    EntitySets = new[] 
+                    EntitySets = new[]
                     {
                         dbModel.ConceptualModel.Container.EntitySets.First(x => x.ElementType == modelBlogConceptualType)
                     }
